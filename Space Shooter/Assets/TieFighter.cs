0b329a7a15a9743fa0cc40288da31300c2e589a5
SkzKatsushiro <deliandenkov@gmail.com>
2018-04-12 20:34:00 +0300

    Added Explosions to player and enemy.

--- Space Shooter/Assets/TieFighter.cs
+++ Space Shooter/Assets/TieFighter.cs
@@ -1,11 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TieFighter : MonoBehaviour {
 
+    [SerializeField]
+    private GameObject explosion;
+
+    [SerializeField]
+    private float explosionDuration;
+
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("TieFighter hit");
+        ContactPoint contact = collision.contacts[0];
+        Hit(contact.point);
+    }
+
+    private void Hit(Vector3 pos)
+    {
+        GameObject exposionParticles = Instantiate(explosion, pos, Quaternion.identity, transform);
+        Destroy(exposionParticles, explosionDuration);
     }
 }
