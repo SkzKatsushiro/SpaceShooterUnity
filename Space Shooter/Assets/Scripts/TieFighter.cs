53107cfcb21c39a8b4309cd11a07481d58f948e6
SkzKatsushiro <deliandenkov@gmail.com>
2018-04-18 23:55:50 +0300

    Addde heath on Player and Regeneration

--- Space Shooter/Assets/Scripts/TieFighter.cs
+++ Space Shooter/Assets/Scripts/TieFighter.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[DisallowMultipleComponent]
 public class TieFighter : MonoBehaviour {
 
     [SerializeField]
@@ -11,16 +9,59 @@ public class TieFighter : MonoBehaviour {
     [SerializeField]
     private float explosionDuration;
 
+    [SerializeField]
+    private int maxHealth;
+
+    [SerializeField]
+    private float regenerationRate;
+
+    [SerializeField]
+    private int regenarationAmount;
+
+    [SerializeField]
+    private int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+
+        InvokeRepeating("Regenerate", regenerationRate, regenerationRate);
+    }
+
+    private void Regenerate()
+    {
+        Debug.Log("Regenerating");
+        currentHealth += regenarationAmount;
+        if (currentHealth >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        } 
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("TieFighter hit");
         ContactPoint contact = collision.contacts[0];
         Hit(contact.point);
     }
 
-    private void Hit(Vector3 pos)
+    private void TakeDamage(Vector3 pos, int damageAmount = 1)
+    {
+        currentHealth -= damageAmount;
+
+        if (currentHealth <= 0)
+        {
+            BlowUp(pos);
+        }
+    }
+
+    private void BlowUp(Vector3 pos)
     {
         GameObject exposionParticles = Instantiate(explosion, pos, Quaternion.identity, transform);
         Destroy(exposionParticles, explosionDuration);
+    }
+
+    private void Hit(Vector3 pos)
+    {
+        TakeDamage(pos);
     }
 }
