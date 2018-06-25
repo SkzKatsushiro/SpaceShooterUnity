a5c7af5ca7ec84f9a016265381002b9fcdb3308f
SkzKatsushiro <deliandenkov@gmail.com>
2018-06-25 21:51:38 +0300

    Fix for targeting
    This reverts commit 05f9dd55f0ac942c7c39c7dc54e5212e39642109.

--- Space Shooter/Assets/Scripts/TargetFinder.cs
+++ Space Shooter/Assets/Scripts/TargetFinder.cs
@@ -1,28 +1,58 @@
 ﻿using UnityEngine;
 
-public class TargetFinder : MonoBehaviour {
+public class TargetFinder : MonoBehaviour
+{
 
     [SerializeField]
     private Transform targetTransform;
 
+    [SerializeField]
+    private bool shouldLookForOtherTargets = true;
+
     public Transform TargetTransform
-    { 
-        get{return targetTransform;}
-        set{ targetTransform = value; }
+    {
+        get { return targetTransform; }
+        set { targetTransform = value; }
+    }
+
+    private void OnEnable()
+    {
+        EventManager.onPlayerDeath += FindMainCamera;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.onPlayerDeath -= FindMainCamera;
     }
 
     public bool FoundTarget()
     {
-        GameObject target = GameObject.FindGameObjectWithTag("Player");
 
-        if (target == null)
+
+        if (targetTransform == null)
         {
-            return false;
+            GameObject target = GameObject.FindGameObjectWithTag("Player");
+
+            if (target == null)
+            {
+                return false;
+            }
+
+            targetTransform = target.transform;
         }
-        else
+        return true;
+    }
+
+    private void FindMainCamera()
+    {
+        if (!shouldLookForOtherTargets)
         {
-            targetTransform = GameObject.FindGameObjectWithTag("Player").transform;
-            return true;
+            return;
         }
+
+        Debug.Log("In find  target");
+        GameObject tmp = GameObject.FindGameObjectWithTag("MainCamera");
+        Debug.Log(tmp);
+        targetTransform = tmp.transform;
     }
 }
