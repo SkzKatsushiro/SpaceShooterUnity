0a63c872be877cbfd0223f3633b40278837073ae
SkzKatsushiro <deliandenkov@gmail.com>
2018-06-25 23:11:39 +0300

    22

--- Space Shooter/Assets/Scripts/Movement.cs
+++ Space Shooter/Assets/Scripts/Movement.cs
@@ -18,25 +18,7 @@ public class Movement : MonoBehaviour {
     [SerializeField]
     private float turnSpeed = 60;
 
-    [SerializeField]
-    public float cameraSensitivity = 90;
-
-    private Transform playerTransform;
-
-    private float rotationX = 0.0f;
-    private float rotationY = 0.0f;
-
-    private bool canMove = false;
-
-    private void OnEnable()
-    {
-        EventManager.onStartGame += LockCursor;
-    }
-
-    private void OnDisable()
-    {
-        EventManager.onStartGame -= LockCursor;
-    }
+    Transform playerTransform;
 
     void Awake()
     {
@@ -45,22 +27,17 @@ public class Movement : MonoBehaviour {
 
     void Update()
     {
-        if (!canMove)
-        {
-            return;
-        }
-            Turn();
-            Thrust();
+        Turn();
+        Thrust();
     }
 
     private void Turn()
     {
-        rotationX += Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
-        rotationY += Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
-        rotationY = Mathf.Clamp(rotationY, -90, 90);
+        float yaw =  turnSpeed * Time.deltaTime * Input.GetAxis(HORIZONTAL);
+        float pitch = turnSpeed * Time.deltaTime * Input.GetAxis(PITCH);
+        float roll = turnSpeed * Time.deltaTime * Input.GetAxis(ROLL);
 
-        playerTransform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
-        playerTransform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+        playerTransform.Rotate(pitch, yaw, -roll);
     }
 
     private void Thrust()
@@ -83,11 +60,5 @@ public class Movement : MonoBehaviour {
                 truster.Activate(false);
             }
         }
-    }
-
-    void LockCursor()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        canMove = true;
     }
 }
