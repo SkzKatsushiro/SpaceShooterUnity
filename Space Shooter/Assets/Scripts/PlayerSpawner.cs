a1bc89c1f6b0fd8a27b946327a0e4beaf250b9ed
SkzKatsushiro <deliandenkov@gmail.com>
2018-06-30 11:39:53 +0300

    - Fixed - player spawner

--- Space Shooter/Assets/Scripts/PlayerSpawner.cs
+++ Space Shooter/Assets/Scripts/PlayerSpawner.cs
@@ -1,5 +1,24 @@
 ﻿using UnityEngine;
 
-public class PlayerSpawner : MonoBehaviour {
+public class PlayerSpawner : MonoBehaviour
+{
 
+    [SerializeField]
+    GameObject playerPrefab;
+
+    private void OnEnable()
+    {
+        EventManager.onStartGame += SpawnPlayer;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.onStartGame -= SpawnPlayer;
+    }
+
+
+    private void SpawnPlayer()
+    {
+        Instantiate(playerPrefab, playerPrefab.transform.position, playerPrefab.transform.rotation);
+    }
 }
